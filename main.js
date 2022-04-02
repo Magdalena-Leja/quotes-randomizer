14a99f106ec5f190ed8d51fe3272511970fa87fe
Magdalena-Leja <leja.magda91@gmail.com>
2022-04-02 19:44:25 +0200

    added generation quotes

---
+++ main.js
@@ -0,0 +1,11 @@
+const API =
+  "https://private-anon-605b4a9238-goquotes.apiary-proxy.com/api/v1/random?count=1";
+
+async function generateQuote() {
+  const response = await fetch(API);
+  const data = await response.json();
+  const quote = data.quotes[0];
+
+  document.querySelector(".quote").textContent = quote.text;
+  document.querySelector(".author").textContent = "- " + quote.author;
+}
